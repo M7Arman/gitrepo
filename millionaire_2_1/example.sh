31f597334fcc00dee27d6b5772f4231fbe8c069e
Arman Kostandyan <armankostandyan@gmail.com>
2014-04-26 00:16:27 +0400

    millionaire_2_1 (Alfa version)

---
+++ millionaire_2_1/example.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+firefox -new-window google.com &
+some_pid=$!
+sleep 15 
+echo $some_pid
+kill -9 $some_pid
+
+#firefox -new-window
+#some_pid=$!
+#firefox -new-tab google.com
+#sleep 15
+#kill -9 $some_pid
+
+#`google-chrome`
+#`google-chrome google.com`
+#sleep 20
+
+#some_pid=$!
+#echo $some_pid
+#sleep 5
+#echo $some_pid
+#kill -9 $some_pid
