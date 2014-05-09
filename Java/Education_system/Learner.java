6cb300fd087842d8795c5d1992ea939b81eb2922
M7Arman <armankostandyan@gmail.com>
2014-05-10 01:14:49 +0400

    Education system #version_1

---
+++ Java/Education_system/Learner.java
@@ -0,0 +1,11 @@
+
+public class Learner extends Person {
+    //TODO: private or protected
+    private int mark;
+    
+// Metods
+    public void lastMark(){
+        System.out.println("last mark of " + name + ":   " + mark);
+    }
+    
+}
---
+++ Java/Education_system/Person.java
@@ -0,0 +1,13 @@
+
+public class Person {
+    //TODO: protected or private
+    protected int age = 0;
+    protected String name = "<name>";
+
+// Metods
+    public void info(){
+        System.out.println("Name: " + name);
+        System.out.println("Age:  " + age);
+    }
+
+}
