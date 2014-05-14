dcdcaec9f674131eb3614fabd3ab6e01fbd29a03
M7Arman <armankostandyan@gmail.com>
2014-05-14 12:14:52 +0400

    Sort Shell and Binary Search (currently without a recursion)

--- Java/Sort_Shell_and_Binary_Search/BinarySearch.java
+++ Java/Sort_Shell_and_Binary_Search/BinarySearch.java
@@ -1,10 +1,10 @@
 
 public class BinarySearch {
-
+    private int mid = 0;                                // sN - searchNumber
+    private int attempt = 0;
     public int search(int[] theArray, int sN) {
         int endPoint = (theArray.length-1);
         int beginPoint = 0;
-        int mid;
 
         while (endPoint > beginPoint){
             mid = (endPoint + beginPoint)/2;
@@ -18,5 +18,21 @@ public class BinarySearch {
         }
         return -1;
     }
-    
+   
+    public int search(int[] theArray, int sN, int beginPoint, int endPoint) {
+        mid = (beginPoint + endPoint)/2;
+
+        if (sN == theArray[mid])
+            return mid;
+
+        if (endPoint > beginPoint)
+            return -1;
+
+        if (sN > theArray[mid])
+            beginPoint = mid+1;
+        else
+            endPoint = mid-1;
+
+        attempt = search(theArray, sN, beginPoint, endPoint);
+    }
 }
