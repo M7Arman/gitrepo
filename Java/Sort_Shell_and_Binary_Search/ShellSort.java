fb580cfde168c54390ab615a67ecc1d28f9be849
M7Arman <armankostandyan@gmail.com>
2014-05-15 12:30:26 +0400

    Sort_Shell_and_Binary_Search add Makefile and Junit test

--- Java/Sort_Shell_and_Binary_Search/ShellSort.java
+++ Java/Sort_Shell_and_Binary_Search/ShellSort.java
@@ -1,24 +1,30 @@
 public class ShellSort {
-public void shell(int[] a) {
-    int step = a.length; 
-    int i = 0;
-    int j = 0;
-    while (step > 0) {
-        step = step/2;
-        for (i = step; i<a.length; i++) {
-            j=i;
-            while ((j-step) >= 0) {
-                if ( a[j] < a[j-step] ){
-                    a[j-step] = a[j-step] + a[j];
-                    a[j] = a[j-step] - a[j];
-                    a[j-step] = a[j-step] - a[j];
-                    j = j-step;
-                } else break;
+    private int temp;
+
+    public void shell(int[] a) {
+        int step = a.length; 
+        int i = 0;
+        int j = 0;
+        while (step > 0) {
+            step = step/2;
+            for (i = step; i<a.length; i++) {
+                j=i;
+                while ((j-step) >= 0) {
+                    if ( a[j] < a[j-step] ){
+                        temp = a[j];
+                        a[j] = a[j-step];
+                        a[j-step] = temp;
+                        j = j-step;
+                    } else break;
+                }
             }
         }
+        for (i=0; i<a.length; i++ ){
+            printArray(i,a);
+        }
     }
-    for (i=0; i<a.length; i++ ){
-        System.out.println("theArray [" + i + "] = " + a[i]);
+
+    private void printArray(int j, int[] array){
+        System.out.println("theArray [" + j + "] = " + array[j]);
     }
-}
 }
