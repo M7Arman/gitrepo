6b68197972e6b14c226b9505d4fcd2e14f927517
M7Arman <armankostandyan@gmail.com>
2014-06-06 01:11:38 +0400

    modify Zuma (modify zuma.js and index.js)

--- Zuma/js/zuma.js
+++ Zuma/js/zuma.js
@@ -1,7 +1,14 @@
 var colorArray = [ "red", "blue", "orange", "green" ];
+var speed = {
+	"patron" : {"x" : "0", "y" : "-3"},
+	"Captive" : {"x" : "3", "y" : "0"},
+}
 
-	var canvas;
-	var context;
+	var canvas = document.getElementById('myCanvas');
+	var context = canvas.getContext('2d');
+	context.canvas.width  = 800/*window.innerWidth / 2*/;
+	context.canvas.height = 600/*window.innerHeight / 2*/; 
+
 
 	var arrayCaptive = {};
 	var arrayCaptiveCounter = 0;
@@ -9,65 +16,83 @@ var colorArray = [ "red", "blue", "orange", "green" ];
 	var arrayPatron = {};
 	var arrayPatronCounter = 0;
 
+	var initialXOfPatron = 0;
+	var initialYOfPatron = 0;
+	var initialXOfCaptive = 0;
+	var initialYOfCaptive = 0;
+
 function play() {
-	console.log("Yeh!");
 	init();
 	// stop
 	// exit
 }
 
+function init () {
+
+	this.factory = new Factory(30,30); 
+	this.frog = new Frog(300,300);
+	this.home = new Home(640,10);
+	canvas.addEventListener("click", this.frog.addPatron, true);
+	//refresh();
+}
+
+////////////////////////////////////////////////////////////
+
 ////////////////////////////////////////////////////////////
 function refresh () {
 	
 	console.log("I");
-	refresh.foo();
-	refresh.fugu();
-
-}
-
-refresh.foo = function () {
-	console.log("LOOKME!");
-		if(false) {
-		clearInterval(id);
-	}
-
-	var id = setInterval(refresh.foo, 1000);
-}
-
-refresh.fugu = function () {
-	console.log("LOOKME.TOO");
 	if(false) {
 		clearInterval(id);
 	}
-	var id = setInterval(refresh.fugu, 1000);
+/*
+	for (i = 0; i < arrayPatron.length; i++) {
+		console.log(i);
+		arrayPatron[i].fire();
+	}*/
+
+	var id = setInterval(refresh, 1000);
 }
 ////////////////////////////////////////////////////////////
 
-function init () {
-	canvas = document.getElementById('myCanvas');
-	context = canvas.getContext('2d');
-	context.canvas.width  = 800/*window.innerWidth / 2*/;
-	context.canvas.height = 600/*window.innerHeight / 2*/; 
+////////////////////////////////////////////////////////////
+function Ball (initialX, initialY) {
 
-	this.factory = new Factory(5,30,30);  // Factory tipi object sarqel
-	this.frog = new Frog(300,300);
-	this.home = new Home(640,10);
-	canvas.addEventListener("click", frog.addPatron, false);
-	refresh();
+	this.x = initialX; 
+	this.y = initialY;
+	this.radius = 15;
+	context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'green';
+    context.fill();
+    context.lineWidth = 5;
+    context.strokeStyle = '#003300';
+    context.stroke();
 }
 
+// runPatron() or fire()
+Ball.prototype.fire = function () {
+	this.x = this.x + speed.patron.x;
+	this.y = this.y + speed.patron.y;
+	drawBall();
+}
 
+Ball.prototype.runCaptive = function () {
+	this.x = this.x + speed.captive.x;
+	this.y = this.y + speed.captive.y;
+	drawBall();
+}
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////
 function Frog (x,y) {
 	this.x = x;
 	this.y = y;
-	//this.patron = new Ball(this.x,this.y);
-	
+		console.log("Frog", x, this.x, y, this.y);
 	var frogImage = new Image();
 	frogImage.src = 'images/frog.png';
 		console.log(frogImage.src);
 	frogImage.onload = function () {
-			console.log(x);
-			console.log(y);
 		context.drawImage(frogImage, x, y,200,200);
 	}
 }
@@ -75,67 +100,45 @@ function Frog (x,y) {
 Frog.prototype.addPatron = function () {
 	var patronXCoord = this.x + 10;
 	var patronYCoord = this.y + 10;
-	console.log("height");
-	arrayPatron[arrayPatronCounter] = new Ball(patronXCoord,patronYCoord);
+	console.log(this.x);
+	console.log(patronXCoord);
 
+	var currentObject = new Ball(patronXCoord,patronYCoord);
+	arrayPatron.push = currentObject;
+	arrayPatronCounter++;
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+function Factory (factoryXCoord, factoryYCoord) {
 
-function Ball (beginCenterX, beginCenterY) {
-
-	var centerX = beginCenterX; 
-	var centerY = beginCenterY;
-	this.radius = 15; // sharavix
-	  context.beginPath();
-      context.arc(centerX, centerY, this.radius, 0, 2 * Math.PI, false);
-      context.fillStyle = 'green';
-      context.fill();
-      context.lineWidth = 5;
-      context.strokeStyle = '#003300';
-      context.stroke();
-}
-
-
-function Factory (amtBall, factoryXCoord, factoryYCoord) {
-	this.amtBall = amtBall;
 	this.xCoord = factoryXCoord;
 	this.yCoord = factoryYCoord;
-	console.log(factoryXCoord);
-	console.log(factoryYCoord);
-	//this.captive = new Ball(this.x,this.y);
+	console.log("factory",this.xCoord, factoryXCoord);
 		var factoryImage = new Image();
 		factoryImage.src = 'images/factory.png';
-		console.log(factoryImage.src);
 		factoryImage.onload = function () {
-			console.log(factoryXCoord);
-			console.log(factoryYCoord);
 			context.drawImage(factoryImage, factoryXCoord, factoryYCoord, 80, 80);
 		}
 }
 
-
-//QUESTION: vortexic kanchem addCaptive metod@
 Factory.prototype.addCaptive = function () {
 	var captiveXCoord = this.xCoord + 10;
 	var captiveYCoord = this.yCoord + 10;
-	console.log("width");
 	arrayCaptive[arrayCaptiveCounter] = new Ball(captiveXCoord, captiveYCoord);
+	arrayCaptiveCounter++;
 }
+//////////////////////////////////////////////////////////////////////////////////////////////
 
 
 function Home (homeXCoord,homeYCoord) {
-	this.xCoord = homeXCoord;
-	this.yCoord = homeYCoord;
-	console.log(this.xCoord,this.yCoord);
-
+	this.x = homeXCoord;
+	this.y = homeYCoord;
+	console.log("Home", this.x, this.y, homeXCoord,homeYCoord);
 	var homeImage=document.createElement('img');
-
-
 	//var homeImage = new Image(); 
 	homeImage.src = 'images/home.png';
-	console.log(homeImage.src);
-	//http://jsfiddle.net/shpaker/uwCFS/
-	//context.fillRect(100,100,500,500);
+
 		homeImage.onload = function () {
 		context.drawImage(homeImage,homeXCoord,homeYCoord);
 		// homeImage.src='images/home.png';
